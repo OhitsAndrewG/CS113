0046b973ad94e0fa95af0041c41cc30ec5a13cf8
andrewgonzales <andrewgonzales@192.168.1.28>
2021-01-28 21:00:55 -0800

    FuzzBuzz re-programmed

--- Lab01FuzzBuzz/src/fuzzBuzz/Main.java
+++ Lab01FuzzBuzz/src/fuzzBuzz/Main.java
@@ -3,24 +3,24 @@ package fuzzBuzz;
 public class Main {
 
 	public static void main(String[] args) {
+//Write a program that prints the numbers from 1 to 100, but for multiples of 3 print “Fizz” instead of the 
+//number and for multiples of 5 print “Buzz”. For numbers that are multiples of both three and five print “FizzBuzz”.
+		
 		
 		for(int i = 1; i <= 100; i++) {
-			if(i % 3 == 0 ) {
-				System.out.println("Fuzz");
-				i++;
-			}
 			
-			if(i % 5 == 0) {
-				System.out.println("Buzz");
-				i++;
-			}
-			
-			if(i % 3 == 0 && i % 5 == 0) {
+			if(i % 3 == 0 && i % 5 == 0 ) {
 				System.out.println("FizzBuzz");
-				i++;
+			}else if(i % 3 == 0) {
+				System.out.println("Fuzz");
+				
+			}else if(i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
 			}
+	
 			
-			System.out.println(i);
 	
 		}
 		
